08726195c2cfb441786b545e0b66e061fa3be2e5
Joey JYH <joeyjyh@gmail.com>
2019-01-17 16:08:42 +0800

    r3.py add

---
+++ r3.py
@@ -0,0 +1,10 @@
+lines = []
+with open('3.txt', 'r', encoding = 'utf-8-sig') as f:
+	for line in f:
+		lines.append(line.strip())
+
+for line in lines:
+	s = line.split(' ')
+	time = s[0][:5]
+	name = s[0][5:]
+	print('在', time, '時, ', name, '說:', s[1:])
